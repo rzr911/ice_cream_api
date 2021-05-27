94b55cadbadf0e889abebf1268319587626c2867
sree <rajan.sreeraj@yahoo.com>
2021-05-27 21:28:57 +0530

    add flow to add cone, baseflavour and topping

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "ice_cream_api.users.apps.UsersConfig",
+    "icecream",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
